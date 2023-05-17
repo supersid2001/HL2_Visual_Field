9e7665e4c5b91c84279dd7a84191117db1f43367
sidd <supersid2001@gmail.com>
2023-05-16 21:43:19 -0400

    Added comments

--- SampleEyeTracking/Assets/LogEyeData.cs
+++ SampleEyeTracking/Assets/LogEyeData.cs
@@ -9,82 +9,97 @@ using Microsoft.MixedReality.Toolkit.Input;
 
 public class LogEyeData : BaseInputHandler, IMixedRealityPointerHandler
 {
+  //The green square for debugging purposes that follows the users eye gaze
   public MoveCanvas canvasObj;
+  //UI Debug texts
   public Text debugText;
   public Text fixationScript;
-  public ExtendedEyeGazeDataProvider extendedEyeGazeDataProvider;
-  public GameObject camera;
 
+  //ExtendedEyeGazeDataProvider to get eye gaze from 
+  public ExtendedEyeGazeDataProvider extendedEyeGazeDataProvider;
+
+  //Camera
+  public GameObject camera;
+  //Attempt to get clicker to work 
   public MixedRealityInputAction ClickerAction;
 
+  //Audio source to play warnings
   public AudioSource source;
 
+  //Audio clip to play if fixation loss exceeds 2 seconds
   public AudioClip fixationLossClip;
 
+  //Audio clip to play if eye gaze is missing for more than 2 seconds
   public AudioClip eyeGazeMissingClip;
 
+  //Audio clip that was supposed to play once clicker is hit
   public AudioClip clickClip;
 
-  GameObject currObject;
-
-  [SerializeField]
-  Material highlightMaterial;
-
-  Material previousMaterial;
-
-  public GetTimeValue script;
-
+  //Floats to keep track of fixation loss and gaze loss time
   float timer_fixation = 0.0f;
   float timer_gaze = 0.0f;
   void Update()
   {
+    //Getting the time for gaze vector
     var timestamp = System.DateTime.Now;
 
+    //Obtain gaze vector for left eye
     var leftGazeReadingInWorldSpace = extendedEyeGazeDataProvider.GetWorldSpaceGazeReading(ExtendedEyeGazeDataProvider.GazeType.Left, timestamp);
+    //Obtain gaze vector for right eye
     var rightGazeReadingInWorldSpace = extendedEyeGazeDataProvider.GetWorldSpaceGazeReading(ExtendedEyeGazeDataProvider.GazeType.Right, timestamp);
+    //Obtain combined gaze vector for both eyes
     var combinedGazeReadingInWorldSpace = extendedEyeGazeDataProvider.GetWorldSpaceGazeReading(ExtendedEyeGazeDataProvider.GazeType.Combined, timestamp);
+    //Script currently looks at left gaze vector but it can be modified
+    //to be any of the above in the future
     if (leftGazeReadingInWorldSpace != null)
     {
+      // Start timer for whether gaze data is being obtained
+      // timer_gaze will be incremented by delta time whenever 
+      // gaze data is not found. If the time exceeds two seconds, then
+      // a sound is played to alert the user
       timer_gaze = 0.0f;
-      //Version 1: Raycast vector and see if it hits object 
-      //Version 2: Get eye position and vector direction to make canvas follow
-      //Currently using version 2 
+
       debugText.text = leftGazeReadingInWorldSpace.GazeDirection.x + "," + leftGazeReadingInWorldSpace.GazeDirection.y + "," + leftGazeReadingInWorldSpace.GazeDirection.z;
+      //Move debug object (green square) to eye gaze 
       canvasObj.MoveCanvasToGazeDistance(leftGazeReadingInWorldSpace.EyePosition, leftGazeReadingInWorldSpace.GazeDirection, 5.0f);
+      //Get a ray from eye position casted to the direction of the gaze direction * 10 
       var ray = new Ray(leftGazeReadingInWorldSpace.EyePosition, leftGazeReadingInWorldSpace.GazeDirection * 10);
       RaycastHit hit;
+      // If the ray hits any collider
       if (Physics.Raycast(ray, out hit, 100000))
       {
-        QuadScript val = hit.transform.gameObject.GetComponent<QuadScript>();
-        if (val != null)
-        {
-          val.addHitPoint(hit.textureCoord.x * 4 - 2, hit.textureCoord.y * 4 - 2);
-          Debug.Log("Hit object at: " + hit.transform.InverseTransformPoint(hit.point));
-        }
         var lasthit = hit.transform.gameObject;
+        // Eye gaze ray hits fixation point
         if (lasthit.tag == "FixationPoint")
         {
+          //Start fixation loss timer
+          // similar to gaze data timer, if timer exceeds
+          // two seconds, then a different sound is played
+          // to alert the user
           timer_fixation = 0.0f;
+          //Pause all audio warnings
           source.Pause();
+          // Update text to inform user that no fixation loss
           fixationScript.text = "No fixation loss";
         }
-        // else if (lasthit.GetComponent<Object_Tag>())
-        // {
-        //   script.UpdateObj(lasthit.GetComponent<Object_Tag>().name);
-        // }
+        //This is if eye gaze vector hits any other collider but fixation point
         else
         {
+          //Update canvas
           fixationScript.text = "Fixation loss";
+          // Increment timer by deltatime
           timer_fixation += Time.deltaTime;
+          //If timer exceeds 2 seconds, play fixation loss clip
           if (timer_fixation >= 2.0f)
           {
             source.Pause();
             source.clip = fixationLossClip;
             source.Play();
           }
-          //script.UpdateObj("Default");
         }
       }
+      // This is if ray hits no collider
+      // It does the same thing as if the ray hits the wrong collider
       else
       {
         timer_fixation += Time.deltaTime;
@@ -95,12 +110,14 @@ public class LogEyeData : BaseInputHandler, IMixedRealityPointerHandler
           source.Play();
         }
         fixationScript.text = "Fixation loss";
-        //script.UpdateObj("Default");
       }
     }
+    //If the eye gaze data is null
     else
     {
+      // Increment gaze timer
       timer_gaze += Time.deltaTime;
+      //If timer exceeds more than two seconds then play warning sound
       if (timer_gaze >= 2.0f)
       {
         source.Pause();
@@ -113,6 +130,7 @@ public class LogEyeData : BaseInputHandler, IMixedRealityPointerHandler
     var combinedGazeReadingInCameraSpace = extendedEyeGazeDataProvider.GetCameraSpaceGazeReading(ExtendedEyeGazeDataProvider.GazeType.Combined, timestamp);
   }
 
+  // Attempts at getting the clicker to work 
   public void OnPointerClicked(MixedRealityPointerEventData eventData)
   {
     if (eventData.MixedRealityInputAction == ClickerAction)
--- SampleEyeTracking/Assets/Point_Spawner.cs
+++ SampleEyeTracking/Assets/Point_Spawner.cs
@@ -5,21 +5,32 @@ using UnityEngine.UI;
 
 public class Point_Spawner : MonoBehaviour
 {
+  // Stimuli point object
   public GameObject imageObject;
+  // Boolean that must be satisfied to spawn stimuli
   bool canSpawn = true;
+  // Brightness override component of HL2
   public BrightnessOverrideComponent bOverride;
 
   void Start()
   {
+    //Set HL2 to maximum brightness initially 
     bOverride.Brightness = 1.0f;
   }
 
+  //This function spawns the fixation point at a specifc x, y position in the
+  // canvas with a specific brightness.
+  // Param1: X position of point
+  // Param2: Y position of point
+  // Param3: Alpha/brightness value of point from a range of 0.0f to 1.0f
   public void SpawnObject(float x_pos, float y_pos, float brightness)
   {
     if (canSpawn)
     {
+      //CHange the position of the object
       imageObject.transform.localPosition = new Vector3(x_pos, y_pos, 0.0f);
       Image image = imageObject.GetComponent<Image>();
+      //Set alpha value to brightness provdied
       var tempColor = image.color;
       tempColor.a = brightness;
       image.color = tempColor;
@@ -27,6 +38,9 @@ public class Point_Spawner : MonoBehaviour
     }
   }
 
+  //This function sets the stimuli active for 0.2 seconds 
+  //After the stimuli is deactivated, it waits for 1 second
+  //before script can spawn any more stimuli
   IEnumerator activateImage()
   {
     imageObject.SetActive(true);
@@ -37,6 +51,7 @@ public class Point_Spawner : MonoBehaviour
     canSpawn = true;
   }
 
+  //Currently set to spawn stimuli at random positions every frame 
   void Update()
   {
     float brightness = Random.Range(0.0f, 1.0f);
